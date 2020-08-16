7a689bf52dcbbe0763a86a3707593b0b090716fe
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-08-16 22:55:18 +0400

    Create more bugs

--- main.py
+++ main.py
@@ -5,6 +5,18 @@
 import eel
 
 
-eel.init("web")
+@eel.expose
+def Converter(number):
 
+    binary = bin(number).replace("0b", "")
+    octal = oct(number).replace("0o", "")
+    hexadecimal = str(hex(number).replace("0x", "")).upper()
+
+    return number
+
+    # print("Bin system: ", binary)
+    # print("Oct system: ", octal)
+    # print("Hex system: ", hexadecimal)
+
+eel.init("web")
 eel.start("main.html", size=(700, 700))
