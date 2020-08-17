665864d62705f783c16791e4a4fe590ca39c3d4a
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-08-17 12:22:05 +0400

    PEP8

--- main.py
+++ main.py
@@ -3,28 +3,20 @@
 
 
 import eel
+
+
 eel.init("web")
 
-# @eel.expose
-def Converter(number):
-
-    binary = bin(int(number)).replace("0b", "")
-    octal = oct(int(number)).replace("0o", "")
-    hexadecimal = str(hex(int(number)).replace("0x", "")).upper()
-
-    return number
-
-    # print("Bin system: ", binary)
-    # print("Oct system: ", octal)
-    # print("Hex system: ", hexadecimal)
 
 @eel.expose
 def binary(number):
     return bin(int(number)).replace("0b", "")
 
+
 @eel.expose
 def Octalf(number):
     return oct(int(number)).replace("0o", "")
+
 
 @eel.expose
 def hexadecimal(number):
