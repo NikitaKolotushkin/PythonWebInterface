69799cfac1dde2b26b55b4fede51fbe365e86db1
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-08-17 20:34:48 +0400

    Added docstrings

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ eel.init("web")  # EEL initialization
 
 @eel.expose
 def binary(number):
+    """This function converts the entered number from the decimal system to binary"""
     return bin(int(number)).replace("0b", "")
 
 
 @eel.expose
 def octal(number):
+    """This function converts the entered number from the decimal system to octal"""
     return oct(int(number)).replace("0o", "")
 
 
 @eel.expose
 def hexadecimal(number):
+    """This function converts the entered number from the decimal system to hexadecimal"""
     return str(hex(int(number)).replace("0x", "")).upper()
 
 
